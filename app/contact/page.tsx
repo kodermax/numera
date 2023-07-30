ea140e1d09c464f49eb4ca579a00fd1a40f58293
Maksim <kodermax@gmail.com>
2023-07-30 13:12:30 +0300

    up

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,14 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Instagram } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
   {
-    icon: <Twitter size={20} />,
+    icon: <Instagram size={20} />,
     href: "https://twitter.com/kodermax",
-    label: "Twitter",
+    label: "Instagram",
     handle: "@kodermax",
   },
   {
